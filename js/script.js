22db160aa8e4d9f99b8e0e46cb4357c669273ad7
Ceej <CJBowman@live.com>
2024-02-06 17:18:11 -0600

    changed app layout and colour add code template

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
-function HelloWorld() {
-    alert("Hello World");
+//get string from page
+//controller function
+function getValue() {
+    document.getElementById("reverseAlert").classList.add("invisible");
+    
+    let string = document.getElementById("regString").value;
+    let rev = reverse(string);
+    
+    displayReverse(rev);
+}
+
+//reverse the string
+//logic function
+function reverse(userString) {
+    let revString = [];
+
+    //reverse string with for loop
+    for (let i = userString.length - 1; i >= 0; i--) {
+        revString += userString[i];
+    }
+    return revString;
+}
+
+//display message
+//view function
+function displayReverse(revString) {
+    //write message to HTML and turn on alert
+    document.getElementById("reverseMessage").innerHTML = `${revString}`;
+    document.getElementById("reverseAlert").classList.remove("invisible");
 }
